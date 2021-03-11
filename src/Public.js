944b6c9a74adeee725c159060ad770703f2393b2
moshe shemoon <moshes@codevalue.com>
2021-03-11 10:21:13 +0200

    work with new backend

--- src/Public.js
+++ src/Public.js
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from "react";
 
 const Public = () => {
-  const [message, setMessage] = useState("");
+  const [message, setMessage] = useState([]);
 
   useEffect(() => {
-    fetch("/public")
+    fetch("http://localhost:7000")
       .then((response) => {
         if (response.ok) {
           return response.json();
@@ -12,11 +12,20 @@ const Public = () => {
           throw new Error("Network response was not ok.");
         }
       })
-      .then((response) => setMessage(response.message))
-      .catch((error) => setMessage(error));
+      .then((response) => setMessage(response))
+      .catch((error) => {
+        console.log(error.toString());
+        setMessage(error.error);
+      });
   }, []);
 
-  return <p>{message}</p>;
+  return (
+    <div>
+      {message.map((item) => (
+        <p key={item.id}>{item.name}</p>
+      ))}
+    </div>
+  );
 };
 
 export default Public;
