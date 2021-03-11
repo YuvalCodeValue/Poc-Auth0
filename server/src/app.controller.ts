2fb79dea2def2f8422643977de1d4c963212c1eb
moshe shemoon <moshes@codevalue.com>
2021-03-11 12:24:00 +0200

    add server

---
+++ server/src/app.controller.ts
@@ -0,0 +1,33 @@
+import { Body, Controller, Get, Param, Post, Req, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { AppService } from './app.service';
+import { JwtAuthGuard } from './auth.guard';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  async findAll(): Promise<any> {
+    console.log('success');
+    return this.appService.findAll();
+  }
+
+  @UseGuards(AuthGuard('jwt'))
+  @Get('private')
+  async findl(@Req() req): Promise<any> {
+    console.log(req.headers);
+    return this.appService.findAll();
+  }
+
+  @Get(':id')
+  async find(@Param('id') id: number): Promise<any> {
+    return this.appService.find(id);
+  }
+
+  @UseGuards(AuthGuard('jwt'))
+  @Post()
+  async create(@Body('item') item: any): Promise<void> {
+    this.appService.create(item);
+  }
+}
